0506b1dc64ac6de36b6238790c409c26a91c7967
NGustavo011 <gsn181200@gmail.com>
2023-07-31 14:42:57 -0300

    feat: add user entity and props

--- src/user/domain/entities/user.entity.ts
+++ src/user/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+export interface UserProps {
+  name: string
+  email: string
+  password: string
+  createdAt?: Date
+}
+
+export class UserEntity {
+  constructor (public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+}
