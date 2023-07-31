252dc7aa9a6ce20f51f8d4f94d574a549cef2c01
NGustavo011 <gsn181200@gmail.com>
2023-07-31 14:26:12 -0300

    refactor: env config module

--- src/shared/infra/env-config/env-config.service.ts
+++ src/shared/infra/env-config/env-config.service.ts
@@ -12,6 +12,6 @@ export class EnvConfigService implements EnvConfig {
   }
 
   getNodeEnv (): string {
-    return this.configService.get<string>('NODE_ENV') ?? 'DEV'
+    return this.configService.get<string>('NODE_ENV') ?? 'development'
   }
 }
