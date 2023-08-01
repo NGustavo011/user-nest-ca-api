079a9927d85cb89eaf851a23c3921bd8c940c6e0
NGustavo011 <gsn181200@gmail.com>
2023-08-01 10:55:38 -0300

    test: user entity integration test validate error in case of invalid email

--- src/user/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/user/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -14,5 +14,17 @@ describe('UserEntity integration tests', () => {
       props = { ...UserDataBuilder({}), name: 'a'.repeat(256) as any }
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
     })
+    it('Should throw an error when creating a user with invalid email', () => {
+      let props: UserProps = { ...UserDataBuilder({}), email: null as any }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+      props = { ...UserDataBuilder({}), email: '' as any }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+      props = { ...UserDataBuilder({}), email: 1 as any }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+      props = { ...UserDataBuilder({}), email: 'test.com' as any }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+      props = { ...UserDataBuilder({}), email: 'a'.repeat(256) as any }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+    })
   })
 })
