a3677334268d783741140179e5c5fe263c09f747
NGustavo011 <gsn181200@gmail.com>
2023-08-01 10:13:15 -0300

    test: user validator email field

--- src/user/domain/validators/__tests__/unit/user-validator.spec.ts
+++ src/user/domain/validators/__tests__/unit/user-validator.spec.ts
@@ -8,6 +8,13 @@ describe('UserValidator unit tests', () => {
     sut = UserValidatorFactory.create()
   })
 
+  it('Valid case for user validator class', () => {
+    const props = UserDataBuilder({})
+    const isValid = sut.validate(props)
+    expect(isValid).toBeTruthy()
+    expect(sut.validatedData).toStrictEqual(new UserRules(props))
+  })
+
   describe('Name field', () => {
     it('Invalidation cases for name field', () => {
       const isValid = sut.validate(null as any)
@@ -60,12 +67,76 @@ describe('UserValidator unit tests', () => {
         ]
       )
     })
+  })
 
-    it('Valid case for name field', () => {
-      const props = UserDataBuilder({})
-      const isValid = sut.validate(props)
-      expect(isValid).toBeTruthy()
-      expect(sut.validatedData).toStrictEqual(new UserRules(props))
+  describe('Email field', () => {
+    it('Invalidation cases for email field', () => {
+      const isValid = sut.validate(null as any)
+      expect(isValid).toBeFalsy()
+      expect(sut.errors.email).toStrictEqual(
+        [
+          'email should not be empty',
+          'email must be an email',
+          'email must be a string',
+          'email must be shorter than or equal to 255 characters'
+        ]
+      )
+    })
+
+    it('Invalidation in case of email is empty', () => {
+      const isValid = sut.validate({
+        ...UserDataBuilder({}),
+        email: ''
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors.email).toStrictEqual(
+        [
+          'email should not be empty',
+          'email must be an email'
+        ]
+      )
+    })
+
+    it('Invalidation in case of email is not a string', () => {
+      const isValid = sut.validate({
+        ...UserDataBuilder({}),
+        email: 1 as any
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors.email).toStrictEqual(
+        [
+          'email must be an email',
+          'email must be a string',
+          'email must be shorter than or equal to 255 characters'
+        ]
+      )
+    })
+
+    it('Invalidation in case of email is not a email format', () => {
+      const isValid = sut.validate({
+        ...UserDataBuilder({}),
+        email: 'test.com'
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors.email).toStrictEqual(
+        [
+          'email must be an email'
+        ]
+      )
+    })
+
+    it('Invalidation in case of email length is greater 255 characters', () => {
+      const isValid = sut.validate({
+        ...UserDataBuilder({}),
+        email: 'a'.repeat(256)
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors.email).toStrictEqual(
+        [
+          'email must be an email',
+          'email must be shorter than or equal to 255 characters'
+        ]
+      )
     })
   })
 })
