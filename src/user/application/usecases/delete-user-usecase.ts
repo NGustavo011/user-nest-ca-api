755e18e793dce98c8002ada4a3b7288532056fa3
NGustavo011 <gsn181200@gmail.com>
2023-08-02 16:28:17 -0300

    feat: add delete-user usecase

---
+++ src/user/application/usecases/delete-user-usecase.ts
@@ -0,0 +1,23 @@
+import { type UserRepository } from '@/user/domain/repositories/user-repository-interface'
+import { type UseCase as DefaultUseCase } from '@/shared/application/usecases/usecase'
+
+export namespace GetUserUseCase {
+
+  export interface Input {
+    id: string
+  }
+
+  export type Output = void
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor (
+      private readonly userRepository: UserRepository.Repository
+    ) {
+    }
+
+    async execute (input: Input): Promise<Output> {
+      const { id } = input
+      await this.userRepository.delete(id)
+    }
+  }
+}
