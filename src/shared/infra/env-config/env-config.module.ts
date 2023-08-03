88c46fec1f7695247f9f5c1fee2eecb5f4ba7614
NGustavo011 <gsn181200@gmail.com>
2023-08-03 09:33:18 -0300

    feat: register usecases in nest module

--- src/shared/infra/env-config/env-config.module.ts
+++ src/shared/infra/env-config/env-config.module.ts
@@ -4,7 +4,9 @@ import { ConfigModule, type ConfigModuleOptions } from '@nestjs/config'
 import { join } from 'path'
 
 @Module({
-  providers: [EnvConfigService]
+  imports: [ConfigModule],
+  providers: [EnvConfigService],
+  exports: [EnvConfigService]
 })
 export class EnvConfigModule extends ConfigModule {
   static forRoot (options: ConfigModuleOptions = {}): DynamicModule {
